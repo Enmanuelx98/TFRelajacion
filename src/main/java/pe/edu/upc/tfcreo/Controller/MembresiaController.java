62563fcecff2404b4ba940e86e04c3ec62451641
Enmanuelx98 <aloisio.forever140@gmail.com>
2025-04-20 16:44:47 -0500

    Update MembresiaController.java

--- src/main/java/pe/edu/upc/tfcreo/Controller/MembresiaController.java
+++ src/main/java/pe/edu/upc/tfcreo/Controller/MembresiaController.java
@@ -1,49 +1,5 @@
 package pe.edu.upc.tfcreo.Controller;
 
-import org.modelmapper.ModelMapper;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
-import pe.edu.upc.tfcreo.Dtos.MembresiaDTO;
-import pe.edu.upc.tfcreo.Entity.Membresia;
-import pe.edu.upc.tfcreo.ServicesInterface.MembresiaServiceInterface;
-
-import java.util.List;
-import java.util.stream.Collectors;
-
-@RestController
-@RequestMapping("/membresia")
 public class MembresiaController {
-    @Autowired
-    private MembresiaServiceInterface membresiaService;
-    //insertar
-    @PostMapping
-    public void insertar(@RequestBody MembresiaDTO dto) {
-        ModelMapper modelMapper = new ModelMapper();
-        Membresia membresia = modelMapper.map(dto, Membresia.class);
-        membresiaService.insertarMembresia(membresia);
-    }
-
-    //modificar
-    @PutMapping
-    public void editar(@RequestBody MembresiaDTO dto) {
-        ModelMapper modelMapper = new ModelMapper();
-        Membresia membresia = modelMapper.map(dto, Membresia.class);
-        membresiaService.updateMembresia(membresia);
-
-    }
-
-    //delete
-    @DeleteMapping("/{id}")
-    public void eliminar(@PathVariable("id") int id) {
-        membresiaService.eliminarMembresia(id);
-    }
-
-    //listar
-    @GetMapping
-    public List<MembresiaDTO> List() {
-        return membresiaService.listarMembresia().stream().map(x -> {
-            ModelMapper m = new ModelMapper();
-            return m.map(x, MembresiaDTO.class);
-        }).collect(Collectors.toList());
-    }
+    
 }
