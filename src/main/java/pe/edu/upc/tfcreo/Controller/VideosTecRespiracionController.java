5e8620237e57e0052c39df396133a999d78e42ae
Enmanuelx98 <aloisio.forever140@gmail.com>
2025-04-20 16:48:20 -0500

    Update VideosTecRespiracionController.java

--- src/main/java/pe/edu/upc/tfcreo/Controller/VideosTecRespiracionController.java
+++ src/main/java/pe/edu/upc/tfcreo/Controller/VideosTecRespiracionController.java
@@ -1,51 +1,5 @@
 package pe.edu.upc.tfcreo.Controller;
 
-import org.modelmapper.ModelMapper;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
-import pe.edu.upc.tfcreo.Dtos.VideosTecnicasRespiracionDTO;
-import pe.edu.upc.tfcreo.Entity.VideosTecnicasRespiracion;
-import pe.edu.upc.tfcreo.ServicesImple.VideosTecRespiracionImple;
-import pe.edu.upc.tfcreo.ServicesInterface.VideosTecRespiracionInterface;
-
-import java.util.List;
-import java.util.stream.Collectors;
-
-@RestController
-@RequestMapping("/VideosTecRespiracion")
 public class VideosTecRespiracionController {
-    @Autowired
-    private VideosTecRespiracionInterface videosTecRespiracionservice;
-
-    //insertar
-    @PostMapping
-    public void insertar(@RequestBody VideosTecnicasRespiracionDTO dto) {
-        ModelMapper modelMapper = new ModelMapper();
-        VideosTecnicasRespiracion videostecnicasrespiracion = modelMapper.map(dto, VideosTecnicasRespiracion.class);
-        videosTecRespiracionservice.insertarVideosTecRespiracion(videostecnicasrespiracion);
-    }
-
-    //modificar
-    @PutMapping
-    public void editar(@RequestBody VideosTecnicasRespiracionDTO dto) {
-        ModelMapper modelMapper = new ModelMapper();
-        VideosTecnicasRespiracion videostecnicasrespiracion = modelMapper.map(dto, VideosTecnicasRespiracion.class);
-        videosTecRespiracionservice.updateVideosTecRespiracion(videostecnicasrespiracion);
-
-    }
-
-    //delete
-    @DeleteMapping("/{id}")
-    public void eliminar(@PathVariable("id") int id) {
-        videosTecRespiracionservice.eliminarVideosTecRespiracion(id);
-    }
-
-    //listar
-    @GetMapping
-    public List<VideosTecnicasRespiracionDTO> List() {
-        return videosTecRespiracionservice.listarVideosTecRespiracion().stream().map(x -> {
-            ModelMapper m = new ModelMapper();
-            return m.map(x, VideosTecnicasRespiracionDTO.class);
-        }).collect(Collectors.toList());
-    }
+   
 }
