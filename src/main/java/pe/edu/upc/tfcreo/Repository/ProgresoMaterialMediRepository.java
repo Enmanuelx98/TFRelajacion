0933dddfd33e51aefe603d08bd1f447ece5374b0
WilliamRG2004 <willrg2323@gmail.com>
2025-04-21 18:01:51 -0500

    Update ProgresoMaterialMediRepository.java

--- src/main/java/pe/edu/upc/tfcreo/Repository/ProgresoMaterialMediRepository.java
+++ src/main/java/pe/edu/upc/tfcreo/Repository/ProgresoMaterialMediRepository.java
@@ -5,4 +5,7 @@ import org.springframework.stereotype.Repository;
 import pe.edu.upc.tfcreo.Entity.ProgresoMaterialMedi;
 @Repository
 public interface ProgresoMaterialMediRepository extends JpaRepository<ProgresoMaterialMedi,Integer> {
+  @Query("Select a from MusicaRelajacion  a where a.nombreMusica like  %:n%")
+    public List<ProgresoMaterialMedi> BuscarPorNombre(String n);
+
 }
