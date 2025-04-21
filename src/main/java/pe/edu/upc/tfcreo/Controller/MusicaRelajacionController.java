acd88bf4899b7f5318278a74e826df757f29f3c5
WilliamRG2004 <willrg2323@gmail.com>
2025-04-20 20:46:19 -0500

    Update MusicaRelajacionController.java

--- src/main/java/pe/edu/upc/tfcreo/Controller/MusicaRelajacionController.java
+++ src/main/java/pe/edu/upc/tfcreo/Controller/MusicaRelajacionController.java
@@ -1,5 +1,49 @@
+
 package pe.edu.upc.tfcreo.Controller;
 
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import pe.edu.upc.tfcreo.Dtos.MusicaRelajacionDTO;
+import pe.edu.upc.tfcreo.Entity.MusicaRelajacion;
+import pe.edu.upc.tfcreo.ServicesInterface.MusicaRelajacionInterface;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RestController
+@RequestMapping("/musicarelax")
 public class MusicaRelajacionController {
-   
+    @Autowired
+    private MusicaRelajacionInterface musicaRelajacionservice;
+    @PostMapping
+    public void insertar(@RequestBody MusicaRelajacionDTO dto) {
+        ModelMapper modelMapper = new ModelMapper();
+        MusicaRelajacion musicarelajacion = modelMapper.map(dto, MusicaRelajacion.class);
+        musicaRelajacionservice.insertarMusicaRelax(musicarelajacion);
+    }
+
+    //modificar
+    @PutMapping
+    public void editar(@RequestBody MusicaRelajacionDTO dto) {
+        ModelMapper modelMapper = new ModelMapper();
+        MusicaRelajacion musicarelajacion = modelMapper.map(dto, MusicaRelajacion.class);
+        musicaRelajacionservice.updateMusicaRelax(musicarelajacion);
+
+    }
+
+    //delete
+    @DeleteMapping("/{id}")
+    public void eliminar(@PathVariable("id") int id) {
+        musicaRelajacionservice.eliminarMusicaRelax(id);
+    }
+
+    //listar
+    @GetMapping
+    public List<MusicaRelajacionDTO> List() {
+        return musicaRelajacionservice.listarMusicaRelax().stream().map(x -> {
+            ModelMapper m = new ModelMapper();
+            return m.map(x, MusicaRelajacionDTO.class);
+        }).collect(Collectors.toList());
+    }
 }
