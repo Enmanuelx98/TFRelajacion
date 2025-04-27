b2a2ff8d939a25576248241e17cff5689cdc60fd
Aldo Jair Angulla Santa Cruz <134071559+AldoAngulla2003@users.noreply.github.com>
2025-04-27 12:39:46 -0500

    Update TecMeditacionController.java

--- src/main/java/pe/edu/upc/tfcreo/Controller/TecMeditacionController.java
+++ src/main/java/pe/edu/upc/tfcreo/Controller/TecMeditacionController.java
@@ -3,10 +3,13 @@ package pe.edu.upc.tfcreo.Controller;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+import pe.edu.upc.tfcreo.Dtos.CantidadTecnicaMeditacionPorTipoTerapiaDTO;
 import pe.edu.upc.tfcreo.Dtos.TecnicaMeditacionDTO;
+import pe.edu.upc.tfcreo.Dtos.TecnicaMeditacionPorTipoTerapiaDTO;
 import pe.edu.upc.tfcreo.Entity.TecnicaMeditacion;
 import pe.edu.upc.tfcreo.ServicesInterface.TecMeditacionServiceInterface;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -46,5 +49,30 @@ public class TecMeditacionController {
             return m.map(x, TecnicaMeditacionDTO.class);
         }).collect(Collectors.toList());
     }
-}
 
+    @GetMapping("/buscarTecnicasMeditacionesPorTiposTerapias")
+    public List<TecnicaMeditacionPorTipoTerapiaDTO> tecnicaMeditacionPorTipoTerapia(){
+        List<TecnicaMeditacionPorTipoTerapiaDTO> dtotecnicaMeditacionPorTipoTerapiaList = new ArrayList<>();
+        List<String[]> fila = tecMeditacionService.tecnicaMeditacionPorTipoTerapia();
+        for(String[]columna : fila){
+            TecnicaMeditacionPorTipoTerapiaDTO dtoTecnicaMeditacionPorTipoTerapia = new TecnicaMeditacionPorTipoTerapiaDTO();
+            dtoTecnicaMeditacionPorTipoTerapia.setNombreTecnicaMeditacion(columna[0]);
+            dtoTecnicaMeditacionPorTipoTerapia.setTipoTerapia(columna[1]);
+            dtotecnicaMeditacionPorTipoTerapiaList.add(dtoTecnicaMeditacionPorTipoTerapia);
+        }
+        return dtotecnicaMeditacionPorTipoTerapiaList;
+    }
+
+    @GetMapping("/cantidadesTecnicasMeditacionesPorTiposTerapias")
+    public List<CantidadTecnicaMeditacionPorTipoTerapiaDTO> cantidadTecnicaMeditacionPorTipoTerapia(){
+        List<CantidadTecnicaMeditacionPorTipoTerapiaDTO> dtocantidadTecnicaMeditacionPorTipoTerapiaList = new ArrayList<>();
+        List<String[]> fila = tecMeditacionService.cantidadTecnicasMeditacionPorTipoTerapia();
+        for(String[]columna : fila){
+            CantidadTecnicaMeditacionPorTipoTerapiaDTO dtocantidadTecnicaMeditacionPorTipoTerapia = new CantidadTecnicaMeditacionPorTipoTerapiaDTO();
+            dtocantidadTecnicaMeditacionPorTipoTerapia.setTipoTerapia(columna[0]);
+            dtocantidadTecnicaMeditacionPorTipoTerapia.setCantidadTecnicaMeditacion(Integer.parseInt(columna[1]));
+            dtocantidadTecnicaMeditacionPorTipoTerapiaList.add(dtocantidadTecnicaMeditacionPorTipoTerapia);
+        }
+        return dtocantidadTecnicaMeditacionPorTipoTerapiaList;
+    }
+}
