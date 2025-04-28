1fcbf97dca10842e7c548b39f5202951ab4e12e3
Enmanuelx98 <aloisio.forever140@gmail.com>
2025-04-28 00:36:37 -0500

    Update PorcentajeDTO.java

--- src/main/java/pe/edu/upc/tfcreo/Dtos/PorcentajeDTO.java
+++ src/main/java/pe/edu/upc/tfcreo/Dtos/PorcentajeDTO.java
@@ -4,26 +4,5 @@ public class PorcentajeDTO {
     private int idsesion;
     private double porcentaje;
 
-    public PorcentajeDTO(){}
-
-    public PorcentajeDTO(int idsesion, double porcentaje) {
-        this.idsesion = idsesion;
-        this.porcentaje = porcentaje;
-    }
-
-    public int getIdsesion() {
-        return idsesion;
-    }
-
-    public void setIdsesion(int idsesion) {
-        this.idsesion = idsesion;
-    }
-
-    public double getPorcentaje() {
-        return porcentaje;
-    }
-
-    public void setPorcentaje(double porcentaje) {
-        this.porcentaje = porcentaje;
-    }
+    
 }
