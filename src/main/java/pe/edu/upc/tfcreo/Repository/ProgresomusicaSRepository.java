33a808c938ecc4440c6e4eee911277bb837b187b
Enmanuelx98 <aloisio.forever140@gmail.com>
2025-04-28 00:37:38 -0500

    Update ProgresomusicaSRepository.java

--- src/main/java/pe/edu/upc/tfcreo/Repository/ProgresomusicaSRepository.java
+++ src/main/java/pe/edu/upc/tfcreo/Repository/ProgresomusicaSRepository.java
@@ -10,12 +10,6 @@ import java.util.List;
 
 @Repository
 public interface ProgresomusicaSRepository extends JpaRepository<ProgresomusicaS, Integer> {
-    //Cantidad de videos que estan completados de una sesion
-    @Query(value = "select * from ProgresomusicaS p where p.idsesion_Terapia = :idSesion and completado = true", nativeQuery = true)
-    public List<ProgresomusicaS> quantityMusicaCompletoBySesion(@Param("idSesion") int idSesion);
-
-    //Cantidad de videos por sesion
-    @Query(value = "select * from ProgresomusicaS p where p.idsesion_Terapia = :idSesion", nativeQuery = true)
-    public List<ProgresomusicaS> countMusicaBySesion(@Param("idSesion") int idSesion);
+ 
 
 }
