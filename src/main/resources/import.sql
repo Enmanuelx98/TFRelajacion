6a9cc8a4130243fea198b2b89be612a49a48c4af
Enmanuelx98 <aloisio.forever140@gmail.com>
2025-05-07 07:31:08 -0500

    Update import.sql

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,5 +1,5 @@
-INSERT INTO users (username, password, enabled, correousuario, fechanacimientousuario) VALUES ('user', '$2a$12$GFwjiGCRDg5x3aNVKpMOM.sjvWJK62RET36nJ3aepsE.r3EFQNDbW', true, 'User@gmail.com', '1990-01-01');
-INSERT INTO users (username, password, enabled, correousuario, fechanacimientousuario) VALUES ('admin', '$2a$12$HiVZzilA7cp05vflCtRrOOdC.tno3RPARhLDKJm5xn5ie2xF9qeEa', true, 'Admin@gmail.com', '1998-07-02');
+INSERT INTO users (username, password, enabled, correousuario, fechanacimientousuario) VALUES ('user', 'user', true, 'User@gmail.com', '1990-01-01');
+INSERT INTO users (username, password, enabled, correousuario, fechanacimientousuario) VALUES ('admin', 'admin', true, 'Admin@gmail.com', '1998-07-02');
 
 INSERT INTO roles (rol, user_id) VALUES ('ROLE_CLIENTE', 1);
 INSERT INTO roles (rol, user_id) VALUES ('ROLE_ADMIN', 2);
