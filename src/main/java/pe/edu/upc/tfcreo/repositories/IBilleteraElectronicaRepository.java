7e591e29f7db384b2a99e9061a53dbff3f8a274d
Joel1208 <joelpaucarlinares12@gmail.com>
2025-05-06 22:17:33 -0500

    Se actualizaron todas las entidades con sus relaciones, dto, servicesinterface y serviceimplement

--- src/main/java/pe/edu/upc/tfcreo/repositories/IBilleteraElectronicaRepository.java
+++ src/main/java/pe/edu/upc/tfcreo/repositories/IBilleteraElectronicaRepository.java
@@ -16,7 +16,7 @@ public interface IBilleteraElectronicaRepository extends JpaRepository<Billetera
             " INNER JOIN \n" +
             " billetera_electronicas b ON p.id_pagos = b.id_pagos\n" +
             " WHERE \n" +
-            " p.fecha_pagos >= CURRENT_DATE - INTERVAL '3 months'\n" +
+            "  p.fecha_pagos BETWEEN '2025-02-02' AND '2025-06-02'" +
             " AND b.id_billetera_electronica IS NOT NULL;",nativeQuery = true)
     double montototabilleteraen3meses();
 
